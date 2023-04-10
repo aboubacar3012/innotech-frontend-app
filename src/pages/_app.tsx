8f2d899c5088734d8bf38af4f27c9949bf4072a9
Aboubacar Diall <a.diallo3012@gmail.com>
2023-04-10 23:27:00 +0200

    adding favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,6 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Apprenez à créer des applications web et mobiles de qualité professionnelle"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
